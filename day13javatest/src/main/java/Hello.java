d11bcb5a4d0638d00a80d6758d20476dc0c9f904
watersing <watersheji@163.com>
2022-04-04 01:19:12 +0800

    java第1次提交 待完善  ( 测试)

---
+++ day13javatest/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Hello  class
+ *
+ * @author lst
+ * @version 1.0
+ * @date 2022/4/4 1:17
+ */
+
+public class Hello {
+}
---
+++ day13javatest/src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Test  class
+ *
+ * @author lst
+ * @version 1.0
+ * @date 2022/4/4 1:16
+ */
+
+public class Test {
+}
